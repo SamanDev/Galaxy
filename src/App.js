0a06073a355b0ffa8d4eefb1394d9209a709668b
SamanDev <mac.developers7@gmail.com>
2023-03-22 04:57:09 +0330

    dd

--- src/App.js
+++ src/App.js
@@ -116,7 +116,7 @@ function App(prop) {
 
       $("body").width(viewportWidth + "px");
       $("body").scrollLeft(0);
-      $(".segment lord-icon").each(function () {
+      $(".grid lord-icon").each(function () {
         var ww = $(this).closest(".ui").width();
         if (ww > viewportWidth / 1.5) {
           ww = viewportWidth - 50;
