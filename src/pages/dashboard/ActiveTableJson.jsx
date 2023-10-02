ec6d37687b9b9fb7d8f2ca1579b4cd4186295d04
SamanDev <mac.developers7@gmail.com>
2023-10-02 15:52:15 +0330

    d

--- src/pages/dashboard/ActiveTableJson.jsx
+++ src/pages/dashboard/ActiveTableJson.jsx
@@ -308,7 +308,7 @@ const ActiveTable = (prop) => {
                         $(".swal2-container").html() == "" ||
                         $(".swal2-container").length == 0
                       ) {
-                        MyToastActive(x, prop.handleOpenTable);
+                        //MyToastActive(x, prop.handleOpenTable);
                       }
                       $("#nav-icon1.open:visible").parent().trigger("click");
                     }}
